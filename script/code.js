84b45bb8a33120a8daeebbee1237ccd79f63ca36
mujahidfisher <fishermujahid@gmail.com>
2023-05-22 15:47:03 +0200

    finished calculator

--- script/code.js
+++ script/code.js
@@ -1,77 +1,81 @@
+let output = document.querySelector('#input')
+let answerArray = []
 // removers
-function remove_allvalue(){
-    let input = document.getElementById('input')
-    input.value = ''
+function remove_valueAll(){
+    let output = document.getElementById('input')
+    output.value = ''
 }
-function remove_value(){
-    let input = document.getElementById('input')
-    input.value = (value -1)
+function remove_value() {
+   let output = input.value.split('')
+   output.pop()
+   input.value = output.join('')
 }
 
 // Numbers
 function add_1(){
-    let input = document.getElementById('input')
-    input.value += '1'
+    let output = document.getElementById('input')
+    output.value += '1'
 }
 function add_2(){
-    let input = document.getElementById('input')
-    input.value += '2'
+    let output = document.getElementById('input')
+    output.value += '2'
 }
 function add_3(){
-    let input = document.getElementById('input')
-    input.value += '3'
+    let output = document.getElementById('input')
+    output.value += '3'
 }
 function add_4(){
-    let input = document.getElementById('input')
-    input.value += '4'
+    let output = document.getElementById('input')
+    output.value += '4'
 }
 function add_5(){
-    let input = document.getElementById('input')
-    input.value += '5'
+    let output = document.getElementById('input')
+    output.value += '5'
 }
 function add_6(){
-    let input = document.getElementById('input')
-    input.value += '6'
+    let output = document.getElementById('input')
+    output.value += '6'
 }
 function add_7(){
-    let input = document.getElementById('input')
-    input.value += '7'
+    let output = document.getElementById('input')
+    output.value += '7'
 }
 function add_8(){
-    let input = document.getElementById('input')
-    input.value += '8'
+    let output = document.getElementById('input')
+    output.value += '8'
 }
 function add_9(){
-    let input = document.getElementById('input')
-    input.value += '9'
+    let output = document.getElementById('input')
+    output.value += '9'
 }
 function add_0(){
-    let input = document.getElementById('input')
-    input.value += '0'
+    let output = document.getElementById('input')
+    output.value += '0'
 }
 // operators
 function add_addition(){
-    let input = document.getElementById('input')
-    input.value += '+'
+    let output = document.getElementById('input')
+    output.value += '+'
 }
 function add_subtraction(){
-    let input = document.getElementById('input')
-    input.value += '-'
+    let output = document.getElementById('input')
+    output.value += '-'
 }
 function add_times(){
-    let input = document.getElementById('input')
-    input.value += 'x'
+    let output = document.getElementById('input')
+    output.value += '*'
 }
 function add_divide(){
-    let input = document.getElementById('input')
-    input.value += '/'
+    let output = document.getElementById('input')
+    output.value += '/'
 }
-function add_equal(){
-    let input = document.getElementById('input')
-    input.value += '='
-}
+document.querySelector('.equal').
+addEventListener('click', (e)=>{
+    output.value = eval(output.value)
+})
+
 function add_dot(){
-    let input = document.getElementById('input')
-    input.value += '.'
+    let output = document.getElementById('input')
+    output.value += '.'
 }
 
