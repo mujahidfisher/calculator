9864d136580683b3cc2a63bcf38514fb693c055c
mujahidfisher <fishermujahid@gmail.com>
2023-05-22 15:52:25 +0200

    prettier

--- script/code.js
+++ script/code.js
@@ -1,81 +1,79 @@
-let output = document.querySelector('#input')
-let answerArray = []
+let output = document.querySelector("#input");
+let answerArray = [];
 // removers
-function remove_valueAll(){
-    let output = document.getElementById('input')
-    output.value = ''
+function remove_valueAll() {
+  let output = document.getElementById("input");
+  output.value = "";
 }
 function remove_value() {
-   let output = input.value.split('')
-   output.pop()
-   input.value = output.join('')
+  let output = input.value.split("");
+  output.pop();
+  input.value = output.join("");
 }
 
 // Numbers
-function add_1(){
-    let output = document.getElementById('input')
-    output.value += '1'
+function add_1() {
+  let output = document.getElementById("input");
+  output.value += "1";
 }
-function add_2(){
-    let output = document.getElementById('input')
-    output.value += '2'
+function add_2() {
+  let output = document.getElementById("input");
+  output.value += "2";
 }
-function add_3(){
-    let output = document.getElementById('input')
-    output.value += '3'
+function add_3() {
+  let output = document.getElementById("input");
+  output.value += "3";
 }
-function add_4(){
-    let output = document.getElementById('input')
-    output.value += '4'
+function add_4() {
+  let output = document.getElementById("input");
+  output.value += "4";
 }
-function add_5(){
-    let output = document.getElementById('input')
-    output.value += '5'
+function add_5() {
+  let output = document.getElementById("input");
+  output.value += "5";
 }
-function add_6(){
-    let output = document.getElementById('input')
-    output.value += '6'
+function add_6() {
+  let output = document.getElementById("input");
+  output.value += "6";
 }
-function add_7(){
-    let output = document.getElementById('input')
-    output.value += '7'
+function add_7() {
+  let output = document.getElementById("input");
+  output.value += "7";
 }
-function add_8(){
-    let output = document.getElementById('input')
-    output.value += '8'
+function add_8() {
+  let output = document.getElementById("input");
+  output.value += "8";
 }
-function add_9(){
-    let output = document.getElementById('input')
-    output.value += '9'
+function add_9() {
+  let output = document.getElementById("input");
+  output.value += "9";
 }
-function add_0(){
-    let output = document.getElementById('input')
-    output.value += '0'
+function add_0() {
+  let output = document.getElementById("input");
+  output.value += "0";
 }
 // operators
-function add_addition(){
-    let output = document.getElementById('input')
-    output.value += '+'
+function add_addition() {
+  let output = document.getElementById("input");
+  output.value += "+";
 }
-function add_subtraction(){
-    let output = document.getElementById('input')
-    output.value += '-'
+function add_subtraction() {
+  let output = document.getElementById("input");
+  output.value += "-";
 }
-function add_times(){
-    let output = document.getElementById('input')
-    output.value += '*'
+function add_times() {
+  let output = document.getElementById("input");
+  output.value += "*";
 }
-function add_divide(){
-    let output = document.getElementById('input')
-    output.value += '/'
+function add_divide() {
+  let output = document.getElementById("input");
+  output.value += "/";
 }
-document.querySelector('.equal').
-addEventListener('click', (e)=>{
-    output.value = eval(output.value)
-})
+document.querySelector(".equal").addEventListener("click", (e) => {
+  output.value = eval(output.value);
+});
 
-function add_dot(){
-    let output = document.getElementById('input')
-    output.value += '.'
+function add_dot() {
+  let output = document.getElementById("input");
+  output.value += ".";
 }
-
