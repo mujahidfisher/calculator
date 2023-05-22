11fafad8ee5f260bfb69b2f46978480f36b06764
mujahidfisher <fishermujahid@gmail.com>
2023-05-22 12:29:59 +0200

    first half

---
+++ script/code.js
@@ -0,0 +1,77 @@
+// removers
+function remove_allvalue(){
+    let input = document.getElementById('input')
+    input.value = ''
+}
+function remove_value(){
+    let input = document.getElementById('input')
+    input.value = (value -1)
+}
+
+// Numbers
+function add_1(){
+    let input = document.getElementById('input')
+    input.value += '1'
+}
+function add_2(){
+    let input = document.getElementById('input')
+    input.value += '2'
+}
+function add_3(){
+    let input = document.getElementById('input')
+    input.value += '3'
+}
+function add_4(){
+    let input = document.getElementById('input')
+    input.value += '4'
+}
+function add_5(){
+    let input = document.getElementById('input')
+    input.value += '5'
+}
+function add_6(){
+    let input = document.getElementById('input')
+    input.value += '6'
+}
+function add_7(){
+    let input = document.getElementById('input')
+    input.value += '7'
+}
+function add_8(){
+    let input = document.getElementById('input')
+    input.value += '8'
+}
+function add_9(){
+    let input = document.getElementById('input')
+    input.value += '9'
+}
+function add_0(){
+    let input = document.getElementById('input')
+    input.value += '0'
+}
+// operators
+function add_addition(){
+    let input = document.getElementById('input')
+    input.value += '+'
+}
+function add_subtraction(){
+    let input = document.getElementById('input')
+    input.value += '-'
+}
+function add_times(){
+    let input = document.getElementById('input')
+    input.value += 'x'
+}
+function add_divide(){
+    let input = document.getElementById('input')
+    input.value += '/'
+}
+function add_equal(){
+    let input = document.getElementById('input')
+    input.value += '='
+}
+function add_dot(){
+    let input = document.getElementById('input')
+    input.value += '.'
+}
+
